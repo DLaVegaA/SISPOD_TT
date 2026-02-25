5624a00e6fbc47937eb3b397de02c0122a7dd06e
ImaTrunks5 <ian_imanol@outlook.com>
2026-02-24 23:29:15 -0600

    Se configuro scripts para importar y eliminar seeder

--- backend/src/seed/seeder.ts
+++ backend/src/seed/seeder.ts
@@ -1,3 +1,4 @@
+import { exit } from 'node:process'
 import {sequelize} from '../config/database';
 import roles from './roles';
 import usuarios from './usuarios';
@@ -6,13 +7,40 @@ import { Usuario, Role } from '../models';
 
 const importarDatos = async() =>{
     try{
-       await sequelize.authenticate()
+       await sequelize.authenticate();
 
-       await sequelize.sync()
+       await sequelize.sync();
 
-       await Role.bulkCreate(roles)
+       await Role.bulkCreate(roles,{
+            ignoreDuplicates:true
+       });
     //    await 
+       await Usuario.bulkCreate(usuarios,{
+            ignoreDuplicates:true
+       });
+
+       console.log('Datos importados correctamente');
     }catch(error){
         console.log(error)
+        exit(1);
     }
+}
+
+const eliminarDatos = async() =>{
+    try{
+        await sequelize.sync({force:true})
+        console.log('Datos Eliminados Correctamente')
+    }catch(error){
+        console.log(error)
+        exit(1)
+    }
+}
+
+const comando = process.argv[2];
+
+if(comando === "-i"){
+    importarDatos();
+}
+if(comando === "-e"){
+    eliminarDatos();
 }
